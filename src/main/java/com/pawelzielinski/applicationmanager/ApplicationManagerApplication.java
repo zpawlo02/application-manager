0c5a7570098afb44b57b275ab91eae79bc72406a
pawel.zielinski <pawel.zielinski@comtronic.pl>
2022-06-06 15:37:50 +0200

    Initial commit

---
+++ src/main/java/com/pawelzielinski/applicationmanager/ApplicationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pawelzielinski.applicationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationManagerApplication.class, args);
+	}
+
+}
